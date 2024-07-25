d0bee7aff980119a4c631b22175a3601778bb6dc
Anton <132255178+dxfh00@users.noreply.github.com>
2024-07-25 10:53:45 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name='Anton'
+age=44
+print('Name:',name)
+print('Age:',age)
+age=str(age//10)+'5' #как понимаю из задания, в этом месте нужно было испльзовать изменение типа данных
+print('New age:',age)
+is_student=2>1
+print('is student:',is_student)
